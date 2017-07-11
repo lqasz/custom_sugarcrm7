65849f6f03f7ea657e612acea4a52cd3d80741a1
lqasz <lucas.osuch@wp.pl>
2017-07-11 17:22:23 +0200

    PCL - official finish

--- modules/Cases/clients/base/views/record/record.js
+++ modules/Cases/clients/base/views/record/record.js
@@ -1,8 +1,8 @@
 ({
     extendsFrom: "RecordView",
     id: 'PCLEdit',
-    pclView: undefined,
-    pclModel: {},
+    pclView: undefined, // main view action
+    pclModel: {}, // main model
     events: _.extend({}, this.events, {
         'blur .description': 'addDescription',
         'blur .pcl-comment-text': 'addComment',
@@ -16,18 +16,25 @@
     initialize: function(options) {
         this._super('initialize', [options]);
         var self = this;
-            pclSections = app.lang.getAppListStrings('pcl_sections_list');
+            pclSections = app.lang.getAppListStrings('pcl_sections_list'); // get all ections fro DropDown list
 
-        self.pclView = options.context.get("action");
+        self.pclView = options.context.get("action"); // current view action
+        // on synchronize complete event in collection 
         self.collection.on('data:sync:complete', function() {
+            // get all data from db
             app.api.call('GET', 'index.php?entryPoint=getData&PCLData=1&pcl_id='+ self.model.get('id'), null,{
                 success: _.bind(function(data) {
                     self.pclModel = data;
 
+                    /**
+                     * if empty data set then create scheme records for all responsible users
+                     * else save `clean` data to the model
+                     */
                     if(data.empty == true) {
                         self.pclModel.section = {};
 
                         _.each(pclSections, function(value, key) {
+                            // split by responsibles
                             var dropDown = value.split("|"),
                                 subject = dropDown[1],
                                 responsiblesSet = dropDown[0].split(",");
@@ -40,6 +47,7 @@
                             for(var i = 0; i < responsiblesSet.length; i++) {
                                 var responsibleKey = responsiblesSet[i].toLowerCase().trim();
 
+                                // add scheme to the empty main model
                                 self.pclModel.section[key].responsibles[i] = {
                                     "id": self.pclModel.users[responsibleKey].id,
                                     "name": self.pclModel.users[responsibleKey].name,
@@ -49,12 +57,12 @@
                                     "iter": i
                                 };
                             }
-                        });
+                        }); // each
                     } else {
                         self.pclModel.section = JSON.parse(data.section.replace(/&quot;/g,'"'));
                     }
                     
-                    self.model.trigger('addToSelect');
+                    self.model.trigger('addToSelect'); // trigger event in model
                 })
             });
         }, self);
@@ -64,6 +72,9 @@
         }, self);
     },
 
+    /**
+     * Change responsible person
+     */
     changePerson: function(e) {
         var self = this,
             data = $(e.currentTarget).data(),
@@ -77,9 +88,12 @@
                     'responsible': key,
                 };
             }
-        });
+        }); // each
     },
 
+    /**
+     * Change verification stage
+     */
     changeVerification: function(e) {
         var self = this,
             oldPCLClass = '',
@@ -110,6 +124,9 @@
         self.pclModel.section[data.name].responsibles[data.iter].verification = newValue;
     },
 
+    /**
+     * Change n/a stage in pcl comments
+     */
     changeNA: function(e) {
         var self = this,
             data = $(e.currentTarget).data(),
@@ -119,6 +136,9 @@
         self.pclModel.answers[data.type].answer[data.id].na = newValue;
     },
 
+    /**
+     * Add a description to the model
+     */
     addDescription: function(e) {
         var self = this,
             PCLClass = '',
@@ -137,6 +157,9 @@
         self.pclModel.section[data.name].responsibles[data.iter].description = newValue;
     },
 
+    /**
+     * Add comment to pcl comments
+     */
     addComment: function(e) {
         var self = this,
             data = $(e.currentTarget).data(),
@@ -146,14 +169,21 @@
         self.pclModel.answers[data.type].answer[data.id].text = newValue;
     },
 
-    _render: function() {  
-        this._super('_render', []);
+    /**
+     * Before render function simply hide all dropdowns
+     */
+    _renderHtml:function(){
+        this._super('_renderHtml');
 
         if(app.user.id != "144c39bf-ccc3-65ec-2023-5407f7975b91") {
             this.$el.find('.btn[name="pcl_dropdown"]').hide();
         }
+        return;
     },
-    
+
+    /**
+     * Add a description to the model
+     */
     render: function() {
         this._super('render');
 
@@ -162,30 +192,27 @@
         this.rebuildFields();
 
         var self = this;
+        // goes for each row, find label row and replace it with new label
         this.$el.find(".span11").each(function(index, el) {
             var label = $(el).children().text();
-            $(el).replaceWith(self.returnLabelField(label));
+            $(el).replaceWith(self.returnLabelField(label)); 
         });
 
-        this.$el.find(".pcl-row").attr("disabled", true);
-        this.$el.find(".pcl-subject-remove").addClass("icon-hiden");
+        this.$el.find(".pcl-row").attr("disabled", true); // add disabled attribiute
         if(this.pclView == "edit") {
-            this.$el.find(".not-disabled").removeAttr("disabled");
-            if(app.user.id == "144c39bf-ccc3-65ec-2023-5407f7975b91") {
-                this.$el.find(".pcl-subject-remove").removeClass("icon-hiden");
-            }
+            this.$el.find(".not-disabled").removeAttr("disabled"); // and remove disabled if row has this class
         }
     },
 
     editClicked: function() {
         this.pclView = "edit";
         this.$el.find(".not-disabled").removeAttr("disabled");
-        if(app.user.id == "144c39bf-ccc3-65ec-2023-5407f7975b91") {
-            this.$el.find(".pcl-subject-remove").removeClass("icon-hiden");
-        }
         this._super('editClicked');
     },
 
+    /**
+     * Add new html fields to the view
+     */
     rebuildFields: function() {
         var self = this;
 
@@ -202,6 +229,7 @@
                 if(section[0] != 5) {
                     html += '<div class="span11 record-cell"><b>'+key+' '+value.subject+'</b></div>';
                     
+                    // add sections fields, represented in models structure
                     for(var i = 0; i < countResponsibles; i++) {
                         var className = "next",
                             n = 12 / countResponsibles;
@@ -225,7 +253,7 @@
                 $recordPanel.append(html);
 
                 iter++;
-            });
+            }); // each
 
             $recordPanel = self.$el.find(".record-panel:eq(5)").children('.record-panel-content');
             _.each(this.pclModel.answers, function(answer, type) {
@@ -236,6 +264,9 @@
         }
     },
 
+    /**
+     * Return html fields which represents comments in model
+     */
     returnCommentField: function(answer, type) {
         var self = this,
             html = '<div class="panel_body">',
@@ -279,6 +310,9 @@
         return html;
     },
 
+    /**
+     * Return html responsible field represented in model
+     */
     returnResponsibleField: function(responsible, dataName, iter) {
         var self = this,
             options = '';
@@ -309,12 +343,15 @@
         return string;
     },
 
+    /**
+     * Return html verification field represented in model
+     */
     returnVerificationField: function(dataName, iter) {
         var options = '',
             PCLClass = "pcl-black",
             responsible = this.pclModel.section[dataName].responsibles[iter],
             disabled = (responsible.id == app.user.id) ? "not-disabled" : "",
-            verification = responsible.verification,
+            verification = responsible.verification, // set default verification
             optionsObject = {
                "PUSTY": "",
                "TAK": "TAK",
@@ -356,6 +393,9 @@
         return string;
     },
 
+    /**
+     * Return simple description field
+     */
     returnDescriptionField: function(dataName, iter) {
         var PCLClass = '',
             responsible = this.pclModel.section[dataName].responsibles[iter],
@@ -388,6 +428,9 @@
         return '<div class="span12 record-cell record-label"><b class="label-bold">'+ label +'</b></div>';
     },
 
+    /**
+     * Send specific data to backend sever
+     */
     saveClicked: function() {
         var self = this;
         this.$el.find('.btn[name="save_button"]').attr("disabled", true);
@@ -403,11 +446,10 @@
                 JSONpclComments: self.pclModel.answers,
             },
             success: function(data) {
-                console.info(data);
                 self.pclView = "detail";
                 self._super('saveClicked');
             },
-        });
+        }); // ajax
     },
 
     cancelClicked: function() {
