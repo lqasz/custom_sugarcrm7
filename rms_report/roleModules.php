b7ea077fc6b065b241b85ddacc6b72faef698c75
lqasz <lucas.osuch@wp.pl>
2017-08-30 21:14:07 +0200

    RMSReport - get all user tasks from task table v1.1

---
+++ rms_report/roleModules.php
@@ -0,0 +1,711 @@
+<?php
+
+	$role_modules = array(
+		"Assistant Dev" => array(
+			"modules" => array(
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 0.25,
+				),
+			),
+
+			"tasks" => array(
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Assistant FA" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 1,
+				),
+			),
+
+			"tasks" => array(
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Contact",
+					"weight" => 1,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+			),
+		), 
+		"Assistant PC" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 0.5,
+				),
+			),
+
+			"tasks" => array(
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		), 
+		"Assistant QS" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 1,
+				),
+			),
+
+			"tasks" => array(
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Junior Manager FA" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 1,
+				),
+			),
+
+			"tasks" => array(
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Contact",
+					"weight" => 1,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+			),
+		),
+		"Junior Manager PC" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 0.5,
+				),
+			),
+
+			"tasks" => array(
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Junior Manager QS" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 1,
+				),
+			),
+
+			"tasks" => array(
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Manager Dev" => array(
+			"modules" => array(
+				"calls" => array(
+					"label" => "Calls",
+					"weight" => 0.5,
+				),
+				"meetings" => array(
+					"label" => "Meetings",
+					"weight" => 0.5,
+				),
+				"prospects" => array(
+					"label" => "Targets",
+					"weight" => 1,
+				),
+				"leads" => array(
+					"label" => "Leads",
+					"weight" => 1,
+				),
+				"opportunities" => array(
+					"label" => "Opportunities",
+					"weight" => 1,
+				),
+				"ac_fee_proposals" => array(
+					"label" => "Fee Proposals",
+					"weight" => 1,
+				),
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 0.25,
+				),
+			),
+
+			"tasks" => array(
+				"prospects" => array(
+					"label" => "Target",
+					"weight" => 1,
+				),
+				"leads" => array(
+					"label" => "Lead",
+					"weight" => 1,
+				),
+				"opportunities" => array(
+					"label" => "Opportunity",
+					"weight" => 1,
+				),
+				"ac_fee_proposals" => array(
+					"label" => "Fee Proposal",
+					"weight" => 1,
+				),
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Manager FA" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"ac_holiday" => array(
+					"label" => "Holidays",
+					"weight" => 0.5,
+				),
+			),
+
+			"tasks" => array(
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Contact",
+					"weight" => 1,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+			),
+		),
+		"Manager PC" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 1,
+				),
+			),
+
+			"tasks" => array(
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Manager QS" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"ac_fee_proposals" => array(
+					"label" => "Fee Proposals",
+					"weight" => 1,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 0.5,
+				),
+			),
+
+			"tasks" => array(
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"ac_fee_proposals" => array(
+					"label" => "Fee Proposal",
+					"weight" => 1,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Senior Manger PC" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 1,
+				),
+				"ac_fee_proposals" => array(
+					"label" => "Fee Proposals",
+					"weight" => 0.5,
+				),
+			),
+
+			"tasks" => array(
+				"ac_fee_proposals" => array(
+					"label" => "Fee Proposal",
+					"weight" => 0.5,
+				),
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		),
+		"Partner" => array(
+			"modules" => array(
+				"contacts" => array(
+					"label" => "Contacts",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Companies",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Buildings",
+					"weight" => 0.5,
+				),
+				"ac_invoices" => array(
+					"label" => "Invoices",
+					"weight" => 1,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"ac_fee_proposals" => array(
+					"label" => "Fee Proposals",
+					"weight" => 1,
+				),
+			),
+
+			"tasks" => array(
+				"contacts" => array(
+					"label" => "Contact",
+					"weight" => 0.5,
+				),
+				"accounts" => array(
+					"label" => "Company",
+					"weight" => 0.5,
+				),
+				"aa_buildings" => array(
+					"label" => "Building",
+					"weight" => 0.5,
+				),
+				"project" => array(
+					"label" => "Project",
+					"weight" => 1,
+				),
+				"aa_departments" => array(
+					"label" => "Department",
+					"weight" => 1,
+				),
+				"aa_persons" => array(
+					"label" => "Person",
+					"weight" => 1,
+				),
+			),
+
+			"activities" => array(
+				"activities" => "Chat",
+				"notifications" => "Notifications",
+				"bugs" => "Bugs",
+				"aa_answers" => "Daily Questions"
+			),
+		), 
+	);
--- rms_report/tasksData.php
+++ rms_report/tasksData.php
@@ -5,13 +5,13 @@ ini_set("display_errors", 1);
 /**
 * Class used to get all data from task table
 */
-class UserRepair 
+class TasksData 
 {
 	private $db;
 	private $user;
 
-	public existing_tasks = array();
-	public not_existing_tasks = array();
+	public $existing_tasks = array();
+	public $not_existing_tasks = array();
 
 	public function __construct($user) 
 	{	
@@ -21,23 +21,27 @@ class UserRepair
 		$this->user = $user;
 		$this->db = DBManagerFactory::getInstance();
 		
-		$this->existing_tasks['overdue_tasks'] = $this->getTasks("AND DATE(`date_due`) < ". $today);
-		$this->existing_tasks['today_tasks'] = $this->getTasks("AND DATE(`date_due`) = ". $today);
-		$this->existing_tasks['tomorow_tasks'] = $this->getTasks("AND DATE(`date_due`) = ". $tomorow);
-		$this->existing_tasks['next_tasks'] = $this->getTasks("AND DATE(`date_due`) > ". $tomorow);
+		$this->existing_tasks['overdue_tasks'] = $this->getTasks("AND DATE(`date_due`) < '{$today}'");
+		$this->existing_tasks['today_tasks'] = $this->getTasks("AND DATE(`date_due`) = '{$today}'");
+		$this->existing_tasks['tomorow_tasks'] = $this->getTasks("AND DATE(`date_due`) = '{$tomorow}'");
+		$this->existing_tasks['next_tasks'] = $this->getTasks("AND DATE(`date_due`) > '{$tomorow}'");
+		$this->existing_tasks['created_tasks'] = $this->getTasks("AND DATE(`date_entered`) = '{$today}'", 'NOT', 0, 0, '`created_by`');
+		$this->existing_tasks['quick_tasks'] = $this->getTasks("", 'NOT', 0, 1);
 
-		$this->not_existing_tasks['closed'] = $this->getTasks("AND DATE(`date_modified`) = ". $today, '');
-		$this->not_existing_tasks['deleted'] = $this->getTasks("AND DATE(`date_modified`) = ". $today, 'NOT', 1);
+		$this->existing_tasks['sum'] = $this->existing_tasks['overdue_tasks'] + $this->existing_tasks['today_tasks'] + $this->existing_tasks['tomorow_tasks'] + $this->existing_tasks['next_tasks'];
+
+		$this->not_existing_tasks['closed'] = $this->getTasks("AND DATE(`date_modified`) = '{$today}'", '');
+		$this->not_existing_tasks['deleted'] = $this->getTasks("AND DATE(`date_modified`) = '{$today}'", 'NOT', 1);
 	}
 
-	public function getTasks($where, $status='NOT', $deleted=0, $new_one=0)
+	public function getTasks($where, $status='NOT', $deleted=0, $new_one=0, $user_activity='`assigned_user_id`')
 	{
 		$sum = 0;
 		$sql_tasks = "SELECT COUNT(`id`) AS `count` 
 						FROM `tasks` 
 							LEFT JOIN `tasks_cstm` 
 								ON(`id`=`id_c`) 
-							WHERE assigned_user_id='{$this->user}' 
+							WHERE $user_activity='{$this->user}' 
 								AND `status` $status LIKE '%Completed%'
 								AND `deleted`=$deleted
 								AND `new_one_c`=$new_one
@@ -47,11 +51,11 @@ class UserRepair
 								AND `generated_c`=0 ";
 		$sql_tasks .= $where;
 
-		$sql_periodic_tasks = "SELECT DISTINCT COUNT(`id`) AS `count`
+		$sql_periodic_tasks = "SELECT DISTINCT `name`
 								FROM `tasks` 
 									LEFT JOIN `tasks_cstm` 
 										ON(`id` = `id_c`) 
-								WHERE `assigned_user_id`='{$this->user}' 
+								WHERE $user_activity='{$this->user}' 
 									AND `status` $status LIKE '%Completed%' 
 									AND `deleted`=$deleted
 									AND `new_one_c`=$new_one
@@ -66,9 +70,14 @@ class UserRepair
 		$sum += $row['count'];
 
 		$result = $this->db->query($sql_periodic_tasks);
-		$row = $this->db->fetchByAssoc($result);
-		$sum += $row['count'];
+		$count = $this->db->getRowCount($result);
+		$sum += $count;
 
 		return $sum;
+	}
+
+	public function getTasksByParentType($parent_type)
+	{
+		return $this->getTasks("AND `parent_type` LIKE '{$parent_type}'");
 	}
 }
